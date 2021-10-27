d6ad56e56616994d462a1db089e0b7410500d48a
Patryk-Szylin <patryk.szylin.dev@gmail.com>
2021-10-27 11:32:20 +0100

    added placeholder for common components

---
+++ src/common/components/TeamIcon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const TeamIcon = () => {
+  return (
+    <View>
+      <Text>Team Icon</Text>
+    </View>
+  );
+};
+
+export default TeamIcon;
+
+const styles = StyleSheet.create({});
